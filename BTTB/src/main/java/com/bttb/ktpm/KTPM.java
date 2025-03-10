14acf966f595f849dc42331057964f291db293c1
Huy <dnmhuy@gmail.com>
2025-03-10 20:25:47 +0700

    hehe

---
+++ BTTB/src/main/java/com/bttb/ktpm/KTPM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.bttb.ktpm;
+
+/**
+ *
+ * @author LEGION
+ */
+public class KTPM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
